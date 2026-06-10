d532be773665d60fbac19203b38e3825a8c6179b
sarweshwaran-rs <sarweshchandran@gmail.com>
2026-06-10 21:47:32 +0530

    refactored the help info with more meaningfull commands and Shortcuts info

--- src/main/java/com/tecs/application/HelpPrinter.java
+++ src/main/java/com/tecs/application/HelpPrinter.java
@@ -7,18 +7,31 @@ public final class HelpPrinter {
         System.out.println(Version.APP_NAME + " " + Version.VERSION);
         System.out.println();
         System.out.println("A terminal-based text editor written in Java.");
-        System.out.println("Usage");
+        System.out.println("UsAGE");
         System.out.println("  jeditor [file]");
         System.out.println("  jeditor --open <filename>");
-        System.out.println("Options:");
-        System.out.println("  -h, --help        Show help");
-        System.out.println("  -v, --version     Show version");
-        System.out.println("  -o, --open FILE   Open file");
+        System.out.println("OPTIONS");
+        System.out.println("  -h, --help        Show this help message and exit");
+        System.out.println("  -v, --version     Show version information and exit");
+        System.out.println("  -o, --open FILE   Open a file");
         System.out.println();
 
         System.out.println("EXAMPLES:");
         System.out.println("  jeditor");
         System.out.println("  jeditor notes.txt");
         System.out.println("  jeditor -o Main.java");
+
+        System.out.println("EXIT CODES");
+        System.out.println("  0 Success");
+        System.out.println("  1 Invalid arguments");
+        System.out.println("  2 File error");
+        System.out.println("  3 Internal error");
+
+        System.out.println("KEYBOARD SHORTCUTS");
+        System.out.println("  CTRL+S Save");
+        System.out.println("  CTRL+O Open File");
+        System.out.println("  CTRL+Q Quit");
+        System.out.println("  CTRL+L Toggle Line Numbers");
+        System.out.println("  Tab About Dialog");
     }
 }
