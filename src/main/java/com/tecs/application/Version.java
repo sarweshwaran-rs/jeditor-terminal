bbddab13c8929af74277ea64374647435ad78787
sarweshwaran-rs <sarweshchandran@gmail.com>
2026-06-10 21:19:55 +0530

    Modified the version number in the VERSION static

--- src/main/java/com/tecs/application/Version.java
+++ src/main/java/com/tecs/application/Version.java
@@ -4,5 +4,5 @@ public final class Version {
     private Version() {}
 
     public static final String APP_NAME = "JEditor";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.2.0";
 }
