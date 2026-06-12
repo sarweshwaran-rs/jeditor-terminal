6e38dd18f1aa9866c6ba83b6d9a32e15113ca3c9
sarweshwaran-rs <sarweshchandran@gmail.com>
2026-06-12 22:52:34 +0530

    Version Updated

--- src/main/java/com/tecs/application/Version.java
+++ src/main/java/com/tecs/application/Version.java
@@ -4,5 +4,9 @@ public final class Version {
     private Version() {}
 
     public static final String APP_NAME = "JEditor";
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.3.0";
+
+    public static String fullVersion() {
+        return APP_NAME + " " + VERSION;
+    }
 }
