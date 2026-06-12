a49dc1461425f18ffe5dd331b7a9bf4616cf4221
sarweshwaran-rs <sarweshchandran@gmail.com>
2026-06-12 22:54:55 +0530

    feat: Editor Constants are grouped in common Editor Constants

---
+++ src/main/java/com/tecs/application/editor/EditorConstants.java
@@ -0,0 +1,10 @@
+package com.tecs.application.editor;
+
+public final class EditorConstants {
+
+    private EditorConstants() { }
+
+    public static final int TAB_SIZE = 4;
+    public static final int STATUS_MESSAGE_TIMEOUT_MS = 5000;
+    public static final int KEY_READ_TIMEOUT_MS = 50;
+}
