ec8f9efbebfb91dd4f0f2d1715950b430179fd14
sarweshwaran-rs <sarweshchandran@gmail.com>
2026-06-15 17:16:55 +0530

    fix(ui): standardize spacing in status bar display

--- src/main/java/com/tecs/application/ui/StatusBar.java
+++ src/main/java/com/tecs/application/ui/StatusBar.java
@@ -6,36 +6,41 @@ public final class StatusBar {
 
     public static String build(Editor editor, int width) {
 
-        String left = "  Ln " 
-                        + (editor.getCursor().getRow() + 1)
-                        + ", Col "
-                        + (editor.getCursor().getColumn() + 1)
-                        + " | " + editor.getDocument().wordCount() 
-                        + " words | "
-                        + editor.getDocument().characterCount()
-                        + " characters ";
+        String left = " Ln "
+                + (editor.getCursor().getRow() + 1)
+                + ", Col "
+                + (editor.getCursor().getColumn() + 1)
+                + " | "
+                + editor.getDocument().wordCount()
+                + " words | "
+                + editor.getDocument().characterCount()
+                + " characters ";
 
-        String fileName = editor.getDocument()
-            .getFilePath() == null 
-            ? "Untitled.txt"
-            : editor.getDocument()
-            .getFilePath()
-            .getFileName()
-            .toString();
-        
+        String fileName = editor.getDocument().getFilePath() == null
+                ? "Untitled.txt"
+                : editor.getDocument()
+                        .getFilePath()
+                        .getFileName()
+                        .toString();
 
-        if(editor.getDocument().isModified()) {
+        if (editor.getDocument().isModified()) {
             fileName += " *";
         }
 
-        String right = "  [" + fileName + "]  ";
+        String right = " [" + fileName + "] ";
 
-        if(left.length() + right.length() > width) {
-            left = " Ln " 
-                + (editor.getCursor().getRow()+1)
-                +" , Col "
-                + (editor.getCursor().getColumn()+1);
+        if (left.length() + right.length() > width) {
+
+            left = " Ln "
+                    + (editor.getCursor().getRow() + 1)
+                    + ", Col "
+                    + (editor.getCursor().getColumn() + 1);
+
+            if (left.length() + right.length() > width) {
+                return left.substring(0, Math.min(left.length(), width));
+            }
         }
+
         int spaces = Math.max(1, width - left.length() - right.length());
 
         return left + " ".repeat(spaces) + right;
